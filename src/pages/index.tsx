dc77fbeae90a0ca18347a1eaf5e4bbe291959a00
Retsam <chess19@gmail.com>
2021-10-24 21:50:43 -0400

    Add a basic listing of posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,46 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql, PageProps } from "gatsby";
 import "./global.css";
 
-const IndexPage = () => {
+type MDXData = {
+    allMdx: {
+        nodes: Array<{
+            id: string;
+            slug: string;
+            frontmatter: { title: string; date: string };
+        }>;
+    };
+};
+const IndexPage = ({ data }: PageProps<MDXData>) => {
+    const posts = data.allMdx.nodes.map((post) => (
+        <li key={post.id}>
+            <h2>
+                <a href={`/blog/${post.slug}`}>{post.frontmatter.title}</a>
+            </h2>
+        </li>
+    ));
     return (
         <Layout pageTitle="Flotsam &amp; Retsam">
-            Hello <a href="#">Test</a>
+            <h1>Posts</h1>
+            <ul>{posts}</ul>
         </Layout>
     );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+    {
+        allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+            nodes {
+                id
+                slug
+                frontmatter {
+                    title
+                    date
+                }
+            }
+        }
+    }
+`;
