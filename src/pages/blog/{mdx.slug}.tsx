32108f09581755acba4366937f981781d6e35227
Retsam <chess19@gmail.com>
2021-10-27 20:04:30 -0400

    Add subtitle

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { graphql, PageProps } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../../components/Layout";
+import "./blogpost.css";
 
-type MDXData = { mdx: { body: string; frontmatter: { title: string } } };
+type MDXData = {
+    mdx: { body: string; frontmatter: { title: string; subtitle: string } };
+};
 const BlogPost = ({ data }: PageProps<MDXData>) => {
     const title = data.mdx.frontmatter.title;
     return (
         <Layout pageTitle={title}>
-            <h1>{title}</h1>
+            <h1 className="post-title">{title}</h1>
+            <h2 className="post-subtitle">{data.mdx.frontmatter.subtitle}</h2>
             <MDXRenderer>{data.mdx.body}</MDXRenderer>
         </Layout>
     );
